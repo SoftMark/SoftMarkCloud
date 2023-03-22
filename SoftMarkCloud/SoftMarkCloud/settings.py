7c2caf7d82bf461bbc88c08fcf2b9bdf3c74f0ff
mykhailo.z <mykhailo.z@influ2.com>
2023-03-22 19:40:47 +0200

    Django secret key hidden

--- SoftMarkCloud/SoftMarkCloud/settings.py
+++ SoftMarkCloud/SoftMarkCloud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-exu-e_!6it&!9_=9uakng^4gh12+(o%3z+7g%e-w!*0pr(&2%n'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
