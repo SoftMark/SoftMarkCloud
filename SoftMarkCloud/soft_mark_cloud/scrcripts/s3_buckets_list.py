d6cc19e84c5a62121a6f0680a6bfe7a84b04e797
Vladyslav <space4sheep@gmail.com>
2023-03-22 13:41:04 +0200

    Created script for getting s3 buckets list from AWS

--- SoftMarkCloud/soft_mark_cloud/scrcripts/s3_buckets_list.py
+++ SoftMarkCloud/soft_mark_cloud/scrcripts/s3_buckets_list.py
@@ -14,30 +14,34 @@ class S3BucketObject:
 
 @dataclass
 class S3Bucket:
-    name: str
     creation_date: datetime.datetime
     bucket_objects: list
 
 
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-)
-
-bucket_list = []
-resp = s3.list_buckets()
-
-for bucket in resp['Buckets']:
-    bucket_name = bucket['Name']
-    bucket_creation_date = bucket['CreationDate']
+def list_s3_buckets_objects(bucket_name: str, s3) -> list:
     bucket_objects_list = []
     response = s3.list_objects(Bucket=bucket_name)
     if 'Contents' in response:
         for obj in response['Contents']:
             bucket_object = S3BucketObject(obj['Key'], obj['Size'], obj['LastModified'])
             bucket_objects_list.append(bucket_object)
-    bucket_list.append(S3Bucket(bucket_name, bucket_creation_date, bucket_objects_list))
+    return bucket_objects_list
 
-for bucket in bucket_list:
-    print(bucket.name)
+
+def dict_s3_buckets(key_id: str, access_key: str) -> dict:
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=key_id,
+        aws_secret_access_key=access_key,
+    )
+    bucket_dict = {}
+    resp = s3.list_buckets()
+    for bucket in resp['Buckets']:
+        bucket_name = bucket['Name']
+        bucket_creation_date = bucket['CreationDate']
+        bucket_dict[bucket_name] = S3Bucket(bucket_creation_date, list_s3_buckets_objects(bucket_name, s3))
+    return bucket_dict
+
+
+if __name__ == '__main__':
+    print(dict_s3_buckets(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY))
