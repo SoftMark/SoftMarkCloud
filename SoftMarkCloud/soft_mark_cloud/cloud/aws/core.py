8633ba1b6cf993a3341cb67516f4a1d7b46c26df
mykhailo.z <mykhailo.z@influ2.com>
2023-03-22 19:13:36 +0200

    Regional and Global AWS clients

--- SoftMarkCloud/soft_mark_cloud/cloud/aws/core.py
+++ SoftMarkCloud/soft_mark_cloud/cloud/aws/core.py
@@ -6,12 +6,27 @@ from soft_mark_cloud.cloud.core import Credentials, CloudClient
 
 @dataclass
 class AWSCredentials(Credentials):
+    """
+    Credentials holder dataclass
+    """
     aws_access_key_id: str
     aws_secret_access_key: str
 
 
-class AWSClient(CloudClient):
-    all_regions = 'eu-central-1'  # TODO: add more regions
+class AWSGlobalClient(CloudClient):
+    """
+    Global AWS client class
+    """
+    def __init__(self, credentials: AWSCredentials, service_name: str):
+        super().__init__(credentials)
+        self.client = boto3.client(service_name, **self.credentials.__dict__)
+
+
+class AWSRegionalClient(CloudClient):
+    """
+    Regional AWS client class
+    """
+    all_regions = 'eu-central-1',  # TODO: add more regions
 
     def __init__(self, credentials: AWSCredentials, service_name: str, region_name: str):
         super().__init__(credentials)
--- SoftMarkCloud/soft_mark_cloud/cloud/aws/services/ec2.py
+++ SoftMarkCloud/soft_mark_cloud/cloud/aws/services/ec2.py
@@ -2,11 +2,14 @@ import datetime
 from dataclasses import dataclass
 from typing import Iterator
 
-from soft_mark_cloud.cloud.aws.core import AWSClient, AWSCredentials
+from soft_mark_cloud.cloud.aws.core import AWSRegionalClient, AWSCredentials
 
 
 @dataclass
 class EC2Instance:
+    """
+    EC2 Instance dataclass
+    """
     instance_id: str
     instance_type: str
     subnet_id: str
@@ -18,33 +21,33 @@ class EC2Instance:
             instance_id=data['InstanceId'],
             instance_type=data['InstanceType'],
             subnet_id=data['SubnetId'],
-            launch_time=data['LaunchTime']
-        )
+            launch_time=data['LaunchTime'])
 
 
-class EC2Client(AWSClient):
+class EC2Client(AWSRegionalClient):
     """
-    This class provides EC2 api functional
-
-    Examples
-    --------
-    >>> from from soft_mark_cloud.cloud.aws.services.ec2 import AWSCredentials, EC2Client
-    ... creds = AWSCredentials(
-    ...     aws_access_key_id='{aws_access_key_id}',
-    ...     aws_secret_access_key='{aws_secret_access_key}'
-    ... )
-    ... client = EC2Client(credentials=creds, region_name='{region_name}')
-    ... list(client.describe_ec2_instances())
-    out:
-        List of EC2Instance class instances.
+    This class provides EC2 API functional
     """
 
     def __init__(self, credentials: AWSCredentials, region_name: str):
         super().__init__(credentials, region_name=region_name, service_name='ec2')
 
+    # TODO: fetch more EC2 instances data
     def describe_ec2_instances(self) -> Iterator[EC2Instance]:
         """
-        Collects ec2 instances
+        Collects ec2 instances for client region
+
+        Examples
+        --------
+        >>> from from soft_mark_cloud.cloud.aws.services.ec2 import AWSCredentials, EC2Client
+        ... creds = AWSCredentials(
+        ...     aws_access_key_id='{aws_access_key_id}',
+        ...     aws_secret_access_key='{aws_secret_access_key}'
+        ... )
+        ... client = EC2Client(credentials=creds, region_name='{region_name}')
+        ... list(client.describe_ec2_instances())
+        out:
+            List of EC2Instance class instances.
         """
         resp: dict = self.client.describe_instances()
         for reservation_data in resp.get('Reservations', []):
