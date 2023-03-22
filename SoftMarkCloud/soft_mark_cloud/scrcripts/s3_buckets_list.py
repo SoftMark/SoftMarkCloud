668162abeb2132ff627b5ac5f4f58a1efde1d489
Vladyslav <space4sheep@gmail.com>
2023-03-22 18:25:08 +0200

    Created script for getting s3 buckets list from AWS

--- SoftMarkCloud/soft_mark_cloud/scrcripts/s3_buckets_list.py
+++ SoftMarkCloud/soft_mark_cloud/scrcripts/s3_buckets_list.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from typing import Dict
+
 from MyPersonalData.S3Buckets_SecreKeys import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 import boto3
@@ -6,7 +8,7 @@ import datetime
 
 
 @dataclass
-class S3BucketContent:
+class S3BucketObject:
     key: str
     size: int
     last_modified: datetime.datetime
@@ -16,14 +18,11 @@ class S3BucketContent:
 class S3Bucket:
     name: str
     creation_date: datetime.datetime
-    bucket_contents: dict
+    bucket_contents: Dict[str, S3BucketObject]
 
     @property
     def bucket_size(self) -> int:
-        total_size = 0
-        for content in self.bucket_contents.values():
-            total_size += content.size
-        return total_size
+        return sum(i.size for i in self.bucket_contents.values())
 
 
 def list_s3_buckets_contents(bucket_name: str, s3) -> dict:
@@ -31,7 +30,7 @@ def list_s3_buckets_contents(bucket_name: str, s3) -> dict:
     response = s3.list_objects(Bucket=bucket_name)
     if 'Contents' in response:
         for obj in response['Contents']:
-            bucket_contents_dict[obj['Key']] = S3BucketContent(obj['Key'], obj['Size'], obj['LastModified'])
+            bucket_contents_dict[obj['Key']] = S3BucketObject(obj['Key'], obj['Size'], obj['LastModified'])
     return bucket_contents_dict
 
 
@@ -44,9 +43,10 @@ def list_s3_buckets(key_id: str, access_key: str) -> list:
     bucket_list = []
     resp = s3.list_buckets()
     for bucket in resp['Buckets']:
-        bucket_name = bucket['Name']
-        bucket_creation_date = bucket['CreationDate']
-        bucket_list.append(S3Bucket(bucket_name, bucket_creation_date, list_s3_buckets_contents(bucket_name, s3)))
+        bucket_list.append(S3Bucket(bucket['Name'],
+                                    bucket['CreationDate'],
+                                    list_s3_buckets_contents(bucket['Name'], s3)
+                                    ))
     return bucket_list
 
 
