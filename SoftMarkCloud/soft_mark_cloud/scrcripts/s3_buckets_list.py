3952e1b34f8f82fec5d301783795c8ffb80e5cec
Vladyslav <space4sheep@gmail.com>
2023-03-22 16:00:06 +0200

    Created script for getting s3 buckets list from AWS

--- SoftMarkCloud/soft_mark_cloud/scrcripts/s3_buckets_list.py
+++ SoftMarkCloud/soft_mark_cloud/scrcripts/s3_buckets_list.py
@@ -6,7 +6,7 @@ import datetime
 
 
 @dataclass
-class S3BucketObject:
+class S3BucketContent:
     key: str
     size: int
     last_modified: datetime.datetime
@@ -16,32 +16,38 @@ class S3BucketObject:
 class S3Bucket:
     name: str
     creation_date: datetime.datetime
-    bucket_objects: list
+    bucket_contents: dict
+
+    @property
+    def bucket_size(self) -> int:
+        total_size = 0
+        for content in self.bucket_contents.values():
+            total_size += content.size
+        return total_size
 
 
-def list_s3_buckets_objects(bucket_name: str, s3) -> list:
-    bucket_objects_list = []
+def list_s3_buckets_contents(bucket_name: str, s3) -> dict:
+    bucket_contents_dict = {}
     response = s3.list_objects(Bucket=bucket_name)
     if 'Contents' in response:
         for obj in response['Contents']:
-            bucket_object = S3BucketObject(obj['Key'], obj['Size'], obj['LastModified'])
-            bucket_objects_list.append(bucket_object)
-    return bucket_objects_list
+            bucket_contents_dict[obj['Key']] = S3BucketContent(obj['Key'], obj['Size'], obj['LastModified'])
+    return bucket_contents_dict
 
 
-def list_s3_buckets(key_id: str, access_key: str) -> dict:
+def list_s3_buckets(key_id: str, access_key: str) -> list:
     s3 = boto3.client(
         's3',
         aws_access_key_id=key_id,
         aws_secret_access_key=access_key,
     )
-    bucket_dict = {}
+    bucket_list = []
     resp = s3.list_buckets()
     for bucket in resp['Buckets']:
         bucket_name = bucket['Name']
         bucket_creation_date = bucket['CreationDate']
-        bucket_dict[bucket_name] = S3Bucket(bucket_name, bucket_creation_date, list_s3_buckets_objects(bucket_name, s3))
-    return bucket_dict
+        bucket_list.append(S3Bucket(bucket_name, bucket_creation_date, list_s3_buckets_contents(bucket_name, s3)))
+    return bucket_list
 
 
 if __name__ == '__main__':
