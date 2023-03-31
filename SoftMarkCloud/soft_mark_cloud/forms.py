937022ae547654b99f9caa15052e06ac718937e6
Vladyslav <space4sheep@gmail.com>
2023-03-31 10:03:19 +0300

    sign_in view is working

--- SoftMarkCloud/soft_mark_cloud/forms.py
+++ SoftMarkCloud/soft_mark_cloud/forms.py
@@ -1,18 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from soft_mark_cloud.models import User
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True, label='Email')
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email')
 
-    def save(self, commit=True):
+    def get_user(self):
         user = super(SignUpForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
         return user
