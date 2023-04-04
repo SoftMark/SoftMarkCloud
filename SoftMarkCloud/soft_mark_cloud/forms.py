a38e88153feb2a2dd1de2a0d4624d8a3ce2af692
Vladkolo <vladkolo2014@gmail.com>
2023-04-04 17:59:12 +0300

    CSS js add for form

--- SoftMarkCloud/soft_mark_cloud/forms.py
+++ SoftMarkCloud/soft_mark_cloud/forms.py
@@ -6,6 +6,14 @@ from soft_mark_cloud.models import User
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
+
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+
     class Meta:
         model = User
         fields = ('username', 'email')
@@ -18,3 +26,9 @@ class SignUpForm(UserCreationForm):
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
