3d969dea26a03e98fbe2fbcdcf5bfb300478859b
mykhailo.z <mykhailo.z@influ2.com>
2023-04-08 12:20:47 +0300

    aws_cloud_data view implemented. tokenization

--- SoftMarkCloud/soft_mark_cloud/views.py
+++ SoftMarkCloud/soft_mark_cloud/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from django.core.exceptions import ObjectDoesNotExist
 
 from soft_mark_cloud.models import AWSCredentials
@@ -8,6 +9,7 @@ from soft_mark_cloud.cloud.aws.collector import AWSCollector
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def aws_cloud_data(request):
     """
     Echo request
@@ -22,7 +24,7 @@ def aws_cloud_data(request):
     >>> import requests
     >>> requests.get(
     ...     url='http://127.0.0.1:8000/api/cloud_data',
-    ...     headers={'Authorization': 'Token db18760656b95b62dfaa788975ab67dd8c062562'} # TODO: Implement tokenization
+    ...     headers={'Authorization': 'Token db18760656b95b62dfaa788975ab67dd8c062562'}
     ... )
     Out:
     <Response [200]>
@@ -31,8 +33,6 @@ def aws_cloud_data(request):
         creds_db = AWSCredentials.objects.get(user=request.user)
     except ObjectDoesNotExist:
         return Response('AWS credentials not provided', status=401)
-
-    # TODO: check if aws creds correct
 
     creds = AWSCreds(
         aws_access_key_id=creds_db.aws_access_key_id,
