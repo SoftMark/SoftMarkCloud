3c9636cc72c025c5020be5d5d90dc6db1962bccf
mykhailo.z <mykhailo.z@influ2.com>
2023-04-19 23:24:49 +0300

    script fix

--- SoftMarkCloud/soft_mark_cloud/static/cloud/js/aws_creds_delete.js
+++ SoftMarkCloud/soft_mark_cloud/static/cloud/js/aws_creds_delete.js
@@ -1,3 +1,29 @@
+// TODO: Implement import from cookie.js
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    var cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+function get_csrftoken() {
+    var token = getCookie('csrftoken')
+    if (token == null) {
+        alert("Error. Sing Out and try again. CSRFToken not found.")
+    }
+    return token
+}
+
 const token = get_csrftoken();
 const del_button = document.getElementById("delete-button");
 
